e9a3f180d9b5a680f745d409bc3a845c7fa23801
502647638 <502647638@G358ZGC2E.logon.ds.ge.com>
2017-04-02 15:07:33 -0400

    Netflix Hystrix Reading Service

---
+++ readingservice/src/test/java/tsk/sp/cloud/hystrix/ReadingserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package tsk.sp.cloud.hystrix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReadingserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
